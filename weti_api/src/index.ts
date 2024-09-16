021385a45d8dde5c42b02cf1c8bcf5d730584009
noctisatrae <noctisatrae@pm.me>
2024-09-16 15:36:03 +0200

    change where the schema are defined

--- weti_api/src/index.ts
+++ weti_api/src/index.ts
@@ -5,6 +5,9 @@ import { bearerAuth } from 'hono/bearer-auth'
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator'
 
+import { watchRequestSchema } from "../db/schemes"
+// import db from "../db";
+
 const TOKEN: string = process.env.TOKEN!;
 const PORT: number = (process.env.PORT === undefined ? 3000 : +process.env.PORT);
 
@@ -16,23 +19,12 @@ if (TOKEN === undefined) {
 const app = new Hono()
 app.use(logger())
 
-const ethereumRPCSchema = z.object({
-  jsonrpc: z.literal("2.0"),
-  method: z.string(),
-  params: z.array(z.string()),
-  id: z.number()
-});
-
-// Define the WatchRequest schema
-const watchReqSchema = z.object({
-  frequency: z.number(),
-  rpc: ethereumRPCSchema
-});
+// console.debug(db);
 
 app.use('/*', bearerAuth({ token: TOKEN }))
 
-app.post('/watch', zValidator("json", watchReqSchema), async (c) => {
-  const body = (await c.req.json()) as z.infer<typeof watchReqSchema>
+app.post('/watch', zValidator("json", watchRequestSchema), async (c) => {
+  const body = (await c.req.json()) as z.infer<typeof watchRequestSchema>
   return c.json({})
 })
 
