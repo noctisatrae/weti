6ea4b40c045b5116ef5a6a4d465ee1a802d30f79
noctisatrae <noctisatrae@pm.me>
2024-09-16 17:24:32 +0200

    start migration before launching the app

--- weti_api/src/index.ts
+++ weti_api/src/index.ts
@@ -1,12 +1,11 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { bearerAuth } from 'hono/bearer-auth'
-
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator'
 
-import { watchRequestSchema } from "../db/schema"
-// import db from "../db";
+import db from "../db";
+import { watchRequest, watchRequestSchema, ethereumRPCSchema } from "../db/schema"
 
 const TOKEN: string = process.env.TOKEN!;
 const PORT: number = (process.env.PORT === undefined ? 3000 : +process.env.PORT);
@@ -23,9 +22,19 @@ app.use(logger())
 
 app.use('/*', bearerAuth({ token: TOKEN }))
 
+db.insert(watchRequest).values({
+  frequency: 500,
+  rpc: {
+    jsonrpc: "2.0",
+    params: ["0x0000"],
+    method: "eth_getbalance",
+    id: 1
+  }
+})
+
 app.post('/watch', zValidator("json", watchRequestSchema), async (c) => {
   const body = (await c.req.json()) as z.infer<typeof watchRequestSchema>
-  return c.json({})
+  return c.json(body);
 })
 
 console.info(`[INFO] Started at http://localhost:${PORT}`);
