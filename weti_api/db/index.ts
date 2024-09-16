c86dfd34d569d8761ed1b9cfacb49fcae8af1fe4
noctisatrae <noctisatrae@pm.me>
2024-09-16 14:50:45 +0200

    basic db instance

---
+++ weti_api/db/index.ts
@@ -0,0 +1,13 @@
+// db.js
+import postgres from 'postgres'
+
+const DB_URL: string = process.env.DB_URL!;
+
+if (DB_URL === undefined) {
+  console.error("DB_URL not set! Quitting...");
+  process.exit();
+}
+
+const sql = postgres(DB_URL) // will use psql environment variables
+
+export default sql
