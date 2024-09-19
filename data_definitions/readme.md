36323c2ed65880b29ebfe4973bafed9670d72671
noctisatrae <noctisatrae@pm.me>
2024-09-19 21:59:32 +0200

    this doenst have any use thanks to drizzle migration

--- data_definitions/readme.md
+++
@@ -1,4 +0,0 @@
-Those SQL schema have been copied out of:
-https://github.com/blockchain-etl/ethereum-etl/tree/develop/schemas/aws
-
-All the credit goes to them :)
--- data_definitions/token_transfer.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS token_transfers (
-    token_address STRING,
-    from_address STRING,
-    to_address STRING,
-    value STRING,
-    transaction_hash STRING,
-    log_index BIGINT,
-    block_number BIGINT
-)
