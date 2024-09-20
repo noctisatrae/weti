fdfe17d458971c73887893ed809b101b8c30b9ab
noctisatrae <noctisatrae@pm.me>
2024-09-20 11:08:01 +0200

    implement POST /get
    The endpoint works by returning all the non-expired job in a descending order.

--- weti_api/src/index.ts
+++ weti_api/src/index.ts
@@ -3,6 +3,7 @@ import { logger } from 'hono/logger'
 import { bearerAuth } from 'hono/bearer-auth'
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator'
+import { asc, gt } from 'drizzle-orm';
 
 import db from "../db";
 import startMigration from '../db/migrate';
@@ -10,6 +11,10 @@ import { watchRequest, watchRequestSchema } from "../db/schema"
 
 const TOKEN: string = process.env.TOKEN!;
 const PORT: number = (process.env.PORT === undefined ? 3000 : +process.env.PORT);
+
+const getJobsSchema = z.object({
+  limit: z.number()
+})
 
 if (TOKEN === undefined) {
   console.error("Token not set! Quitting...");
@@ -22,8 +27,6 @@ await startMigration;
 const app = new Hono()
 app.use(logger())
 
-// console.debug(db);
-
 app.use('/*', bearerAuth({ token: TOKEN }))
 
 app.post('/watch', zValidator("json", watchRequestSchema), async (c) => {
@@ -34,14 +37,29 @@ app.post('/watch', zValidator("json", watchRequestSchema), async (c) => {
     // It works for now, might investigate later
     const result = await db.insert(watchRequest).values(body).returning().execute();
 
-    console.log('Insert result:', result); // Log result to verify insertion
-
     return c.json(body);
   } catch (error) {
     console.error('Error inserting into DB:', error);
     return c.json({ error: 'An error occurred' }, { status: 500 });
   }
 });
+
+app.post('/jobs', zValidator("json", getJobsSchema), async (c) => {
+  try {
+    const body = (await c.req.json()) as z.infer<typeof getJobsSchema>;
+    console.debug(body.limit)
+    const validJobs = await db
+      .select()
+      .from(watchRequest)
+      .where(gt(watchRequest.expiration, new Date().toISOString()))
+      .orderBy(asc(watchRequest.expiration));
+
+    return c.json(validJobs);
+  } catch (error) {
+    console.error("Error getting jobs from DB:", error)
+    return c.json({ error: 'An error occured' }, { status: 500 });
+  }
+})
 
 console.info(`[INFO] Started at http://localhost:${PORT}`);
 export default { 
