9e9efa15a7f24ff8901510bdd8908dac51129975
noctisatrae <noctisatrae@pm.me>
2024-09-26 11:40:11 +0200

    reorganize types (not perfect might change later)

--- weti-queue/types.go
+++ weti-queue/types.go
@@ -1,5 +1,21 @@
 package main
 
+type Job struct {
+	Id         int      `json:"id"`
+	ChainId    uint     `json:"chainId"`
+	Frequency  uint     `json:"frequency"`
+	Expiration string   `json:"expiration"`
+	Provider   Provider `json:"provider"`
+	Rpc        Rpc      `json:"rpc"`
+}
+
+// ? It's an alias so I don't really know if it's needed...
+type UntypedJson map[string]interface{}
+
+type GetJobRequest struct {
+	Limit int `json:"limit"`
+}
+
 type Rpc interface{}
 
 type RpcResponse struct {
