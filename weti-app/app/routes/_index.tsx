3f6d01bb61829795d9b695604fccedbe0a817b7a
noctisatrae <noctisatrae@pm.me>
2024-09-29 17:36:23 +0200

    get balances (the code is surprinsingly small good sign?)

--- weti-app/app/routes/_index.tsx
+++ weti-app/app/routes/_index.tsx
@@ -1,22 +1,32 @@
 import { useFetcher } from "@remix-run/react";
 import { useEffect } from "react";
-import { loader } from "./jobs";
+import { loader } from "./balance";
 
 import { useIsConnected } from "~/hooks/useIsConnected";
+import { GetBalances } from "~/types/alchemy";
+
+import { formatEther } from 'ethers'
+import { useChainId } from "wagmi";
 
 const Index = () => {
   const isConnected = useIsConnected()
+  const chain = useChainId()
   const fetcher = useFetcher<typeof loader>()
 
   useEffect(() => {
     if (isConnected == true) {
-      fetcher.load("/jobs?connected=true")
+      fetcher.load("/balance?connected=true&chain=" + chain)
     }
-  }, [isConnected])
+  }, [isConnected, chain])
 
   return (
     <>
-      {fetcher.data == null && isConnected ? (<p>Loading...</p>) : (<p>{JSON.stringify(fetcher.data)}</p>)}
+      {fetcher.data == undefined || null && isConnected ?
+        (<p>Loading...</p>) :
+        ((fetcher.data as GetBalances).result.tokenBalances.map(balance => 
+          <li key={balance.contractAddress}>{formatEther(balance.tokenBalance)}</li>
+        ))
+      }
     </>
   );
 }
