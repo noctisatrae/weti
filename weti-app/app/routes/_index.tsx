f11da19781d9b177e411fad833ce7b95bfa10b73
noctisatrae <noctisatrae@pm.me>
2024-09-30 11:19:04 +0200

    pass address to the balance loop

--- weti-app/app/routes/_index.tsx
+++ weti-app/app/routes/_index.tsx
@@ -1,34 +1,40 @@
 import { useFetcher } from "@remix-run/react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { loader } from "./balance";
 
 import { useIsConnected } from "~/hooks/useIsConnected";
-import { GetBalances } from "~/types/alchemy";
+import { GetBalances } from "~/types/moralis";
+import { useAccount, useChainId } from "wagmi";
 
-import { formatEther } from 'ethers'
-import { useChainId } from "wagmi";
+import { parseBalance } from '~/lib/'
 
 const Index = () => {
   const isConnected = useIsConnected()
+  const account = useAccount()
   const chain = useChainId()
   const fetcher = useFetcher<typeof loader>()
 
   useEffect(() => {
     if (isConnected == true) {
-      fetcher.load("/balance?connected=true&chain=" + chain)
+      fetcher.load(`/balance?connected=true&chain=${chain}&address=${account.address}`)
     }
   }, [isConnected, chain])
 
   return (
     <>
-      {fetcher.data == undefined || null && isConnected ?
-        (<p>Loading...</p>) :
-        ((fetcher.data as GetBalances).result.tokenBalances.map(balance => 
-          <li key={balance.contractAddress}>{formatEther(balance.tokenBalance)}</li>
-        ))
-      }
+      {fetcher.data == null && fetcher.data == undefined && isConnected ? (
+        <p>Loading...</p>
+      ) : (
+        fetcher.data && (fetcher.data as GetBalances).result? (
+          (fetcher.data as GetBalances).result.map(token => (
+            <li key={token.symbol}><img src={token.logo} alt={token.symbol} />{parseBalance(token.balance, token.decimals)}</li>
+          ))
+        ) : (
+          <p>No balances available</p>
+        )
+      )}
     </>
-  );
+  );  
 }
 
 export default Index;
