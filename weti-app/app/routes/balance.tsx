02d864af4eea306ec29a116371904d3d54b0c863
noctisatrae <noctisatrae@pm.me>
2024-09-30 11:19:29 +0200

    use moralis + pass address

--- weti-app/app/routes/balance.tsx
+++ weti-app/app/routes/balance.tsx
@@ -1,13 +1,17 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node"
-import { base, mainnet } from "viem/chains";
 
 import { Provider, WatchRequest, WatchRequestResponse, WatchRequestResponseError, RpcResponse } from "~/types/";
-import { GetBalances } from "~/types/alchemy";
+import Moralis from "~/types/moralis"
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const chain = url.searchParams.get('chain');
   const isConnected = url.searchParams.get('connected') === 'true';
+  const address = url.searchParams.get('address');
+
+  if (address === null) {
+    return json({ error: "Address was not provided!" }, { status: 400 })
+  }
 
   if (chain === null) {
     return json({ error: 'Chain id not provided' }, { status: 400 })
@@ -27,14 +31,17 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
         frequency: 10,
         // 2 minutes from now
         expiration: new Date(Date.now() + 2 * 60 * 1000).toISOString(),
-        provider: Provider.Alchemy,
+        provider: Provider.Moralis,
         rpc: {
           jsonrpc: "2.0",
-          method: "alchemy_getTokenBalances",
+          method: "eth_getTokenBalances",
           params: [
-            "0x84673f99d9807780ce5Db4c3A980d708535d9604",
+            {
+              "address": address!,
+              "excludeSpam": true,
+            }
           ],
-          id: 42
+          id: 1
         }
       } as WatchRequest),
       headers: {
@@ -48,10 +55,10 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       throw new Error("Failed to create job");
     }
 
-    const {id} = await watchResponse.json() as WatchRequestResponse;
+    const { id } = await watchResponse.json() as WatchRequestResponse;
 
     // 2. Poll the /result endpoint until the job is completed
-    let jobResult: RpcResponse<GetBalances> | null = null;
+    let jobResult: RpcResponse<Moralis> | null = null;
     const pollInterval = 2000; // 2 seconds polling interval
     const maxRetries = 25; // Retry 25 times before giving up
     let retries = 0;
