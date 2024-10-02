64ad1c05000041484f4b9d274394d24df8cc288e
noctisatrae <noctisatrae@pm.me>
2024-10-02 09:52:51 +0200

    metamask api type

---
+++ weti-app/app/types/metamask.ts
@@ -0,0 +1,30 @@
+export interface PriceAPI {
+  [key: string]: unknown;
+  id:                       string;
+  symbol:                   string;
+  name:                     string;
+  price:                    number;
+  marketCap:                number;
+  allTimeHigh:              number;
+  allTimeLow:               number;
+  totalVolume:              number;
+  high1d:                   number;
+  low1d:                    number;
+  circulatingSupply:        number;
+  dilutedMarketCap:         number;
+  marketCapPercentChange1d: number;
+  priceChange1d:            number;
+  pricePercentChange1h:     number;
+  pricePercentChange1d:     number;
+  pricePercentChange7d:     number;
+  pricePercentChange14d:    number;
+  pricePercentChange30d:    number;
+  pricePercentChange200d:   number;
+  pricePercentChange1y:     null;
+  lastUpdated:              string;
+  platforms:                Platforms;
+}
+
+export interface Platforms {
+  [key: string]: string
+}
