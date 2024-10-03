7d401a7e9734f659bd07fd3a98f99254c477ed4c
noctisatrae <noctisatrae@pm.me>
2024-10-03 22:37:05 +0200

    TODO make request

--- weti-app/app/routes/watch.$address.tsx
+++ weti-app/app/routes/watch.$address.tsx
@@ -1,5 +1,8 @@
-import { useParams } from "@remix-run/react";
+import { ActionFunctionArgs } from "@remix-run/node";
+import { json, redirect, useParams } from "@remix-run/react";
+import { add } from "date-fns";
 import React, { useState } from "react";
+import { isAddress } from "viem";
 import CalendarForm from "~/components/CalendarForm";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
@@ -8,6 +11,8 @@ import { Slider } from "~/components/ui/slider";
 
 const Watch = () => {
   const params = useParams();
+  const [address, setAddress] = useState<string>(params.address!);
+  const [utcDate, setUTCDate] = useState<string>(""); 
   const [frequency, setFrequency] = useState<number>(10);
 
   return <>
@@ -17,19 +22,34 @@ const Watch = () => {
         <CardDescription>Creates a job that gives you information about the earnings of a wallet</CardDescription>
       </CardHeader>
       <CardContent>
-        <form action="">
+        <form method="post">
           <div className="grid w-full items-center gap-4">
-            <Input placeholder="Address" value={params.address} />
+            <Input placeholder="Address" name="address" onChange={(e) => setAddress(e.target.value)} defaultValue={params.address} />
             <Slider about="frequency" min={10} max={100} onValueChange={(val) => {
               setFrequency(val[0])
             }} />
-            <CalendarForm/>
-            <Button>Watch wallet every {frequency > 60 ? "1h + " + frequency % 60 + " min." : frequency + " min."}</Button>
+            <CalendarForm name="test" utcDate={utcDate} setUTCDate={setUTCDate} />
+            <input type="hidden" name="utcDate" value={utcDate} />
+            <input type="hidden" name="frequency" value={frequency} />
+            <Button disabled={utcDate.length == 0 || !isAddress(address)}>Watch wallet every {frequency > 60 ? "1h + " + frequency % 60 + " min." : frequency + " min."}</Button>
           </div>
         </form>
       </CardContent>
     </Card>
   </>
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const body = await request.formData();
+  const address = body.get("address")
+  const utcDate = body.get("utcDate")
+  const frequency = body.get("frequency")
+
+  console.debug({address, utcDate, frequency})
+
+  fetch("http://localhost:8000/watch", { method: "POST" })
+
+  return redirect("/job/3")
 }
 
 export default Watch;
