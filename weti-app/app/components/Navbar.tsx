7443b40e7e47651b9b962d8d44dd0337fcb46c16
noctisatrae <noctisatrae@pm.me>
2024-10-05 12:10:30 +0200

    better navbar

--- weti-app/app/components/Navbar.tsx
+++ weti-app/app/components/Navbar.tsx
@@ -1,24 +1,58 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit"
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import React from 'react';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "~/components/ui/navigation-menu";
 import { Link } from "@remix-run/react";
+import { cn } from '~/lib/utils';
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+
+ListItem.displayName = "ListItem";
 
 const Navbar = () => {
-  return <NavigationMenu className="navbar">
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Wallet</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <ConnectButton />
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Watch</NavigationMenuTrigger>
-      <NavigationMenuContent>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-    <ConnectButton accountStatus={"avatar"} chainStatus={"none"} />
-  </NavigationMenuList>
-</NavigationMenu>
-}
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="/" prefetch="intent">
+            <ListItem title="Home">
+            </ListItem>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/watch" prefetch="intent">
+            <ListItem title="Watch">
+            </ListItem>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Wallet</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ConnectButton />
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
 export default Navbar;
