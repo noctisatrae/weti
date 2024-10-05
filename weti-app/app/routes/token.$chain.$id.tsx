037e54c7322ff018c0164f6e352257e9b0d65dca
noctisatrae <noctisatrae@pm.me>
2024-10-05 12:11:15 +0200

    error handling price api

--- weti-app/app/routes/token.$chain.$id.tsx
+++ weti-app/app/routes/token.$chain.$id.tsx
@@ -8,7 +8,12 @@ import { PriceAPI } from "~/types/metamask";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const priceRequest = await fetch(`https://price.api.cx.metamask.io/v1/chains/${params['chain']}/spot-prices/${params['id']}`)
-  return json(await priceRequest.json() as PriceAPI)
+  try {
+    const data = await priceRequest.json();
+    return json(data as PriceAPI)
+  } catch (e) {
+    return json({ error: e  })
+  }  
 }
 
 const Index = () => {
@@ -16,19 +21,21 @@ const Index = () => {
 
   return (
     <Tabs className="p-1 lg:p-5 lg:m-5" defaultValue="metrics">
-      <script src="https://widgets.coingecko.com/gecko-coin-price-chart-widget.js"></script>
       <TabsList>
         <TabsTrigger value="metrics">Key Metrics</TabsTrigger>
         <TabsTrigger value="chart" >Chart</TabsTrigger>
         <TabsTrigger value="trade">Trade</TabsTrigger>
       </TabsList>
       <TabsContent value="metrics">
+        { /* @ts-ignore */ }
         {data.symbol == null ? <p>This token doesn't have public data available...</p> : <KeyMetrics {...data}/>}
       </TabsContent>
       <TabsContent value="chart">
+        { /* @ts-ignore */ }
         {data != null ? <PriceChart id={data.id} /> : <p>Loading chart...</p>}
       </TabsContent>
       <TabsContent value="trade">
+        <p>hello world!</p>
       </TabsContent>
     </Tabs>
   );
