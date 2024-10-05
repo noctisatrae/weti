16f8b83b289ec3c5d75cea975adad6fa4e5af5d1
noctisatrae <noctisatrae@pm.me>
2024-10-05 10:38:16 +0200

    navigation menu

--- weti-app/app/components/Navbar.tsx
+++ weti-app/app/components/Navbar.tsx
@@ -1,9 +1,24 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { Link } from "@remix-run/react";
 
 const Navbar = () => {
-  return <nav>
-    <ConnectButton/>
-  </nav>
+  return <NavigationMenu className="navbar">
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Wallet</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <ConnectButton />
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Watch</NavigationMenuTrigger>
+      <NavigationMenuContent>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+    <ConnectButton accountStatus={"avatar"} chainStatus={"none"} />
+  </NavigationMenuList>
+</NavigationMenu>
 }
 
 export default Navbar;
