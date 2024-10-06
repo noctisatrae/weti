be1efe205a098ca21cddd9eb9eae68c93f155b1d
noctisatrae <noctisatrae@pm.me>
2024-10-06 21:13:41 +0200

    error handling

--- weti-app/app/routes/tokens.tsx
+++ weti-app/app/routes/tokens.tsx
@@ -1,9 +1,21 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { TokenList } from "~/types";
 
-const loader = async ({ params }: LoaderFunctionArgs) => {
-  const req = await fetch("https://account.api.cx.metamask.io/networks/1/tokens");
-  return json((await req.json()) as TokenList[])
+const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const chainId = url.searchParams.get("chainId") || "1"; // Default to Ethereum mainnet if not provided
+
+  const req = await fetch(`https://account.api.cx.metamask.io/networks/${chainId}/tokens`);
+  try {
+    const res = await req.json()
+    return json(res as TokenList[])
+  } catch (e) {
+    return json({ error: e }, { status: 500 })
+  }
 }
+
+export const shouldRevalidate = () => {
+  return false;
+} 
 
 export default loader;
